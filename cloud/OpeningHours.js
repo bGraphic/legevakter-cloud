b32f4ce6d6e2184004c0c054c9184abf1a5417af
Tom Erik Støwer <testower@gmail.com>
2013-04-30 21:12:17 +0200

    Basic logic for OpeningHours module is done. Things to consider: start day of week, handle before/after logic around start/end of week. Todo: formatting functionality, and importing functions to convert from old format (helsenorge.no) to this better format for HealthServices

--- cloud/OpeningHours.js
+++ cloud/OpeningHours.js
@@ -1,22 +1,92 @@
 /**
- * OpeningHours.js - Parse Cloud Module for "Legevakter"
- *
+ * OpeningHours.js - Handles business hour logic and formatting
+ * Designed to work in Parse.com, backbone and Node.js environments
  * 
  * Copyright: Tom Erik Støwer 2013
  * E-mail: testower@gmail.com
  *
  */
 
- var moment = require('moment');
- var twix = require('cloud/twix.min.js');
- 
- var OpeningHours = Parse.Object.extend("OpeningHours", {
- 	hello: function() {
- 		console.log("Hello");
- 	}
- 	// instance
- }, {
- 
- 	// class
- });
- exports.OpeningHours = OpeningHours;
+// This module requires 'moment.js' => http://momentjs.com/
+var moment = require('moment');
+
+/**
+ * OpeningHourRange
+ */
+function OpeningHourRange (attributes) {
+	this.start = attributes.start;
+	this.end = attributes.end;	
+}
+
+OpeningHourRange.prototype.containsDate = function (date) {
+	
+	var timeToCheck = new OpeningHourTime({
+		day: moment(date).day(),
+		hour: moment(date).hour(),
+		minute: moment(date).minute()
+	});
+
+	return timeToCheck.isAfter(this.start) && timeToCheck.isBefore(this.end);
+}
+
+/**
+ * OpeningHourTime
+ */
+function OpeningHourTime (attributes) {
+	this.day = attributes.day;
+	this.hour = attributes.hour;
+	this.minute = attributes.minute;
+}
+
+OpeningHourTime.prototype.isAfter = function(time) {
+	if ( this.day > time.day )
+		return true;
+	else if ( this.day == time.day ) {
+		if ( this.hour > time.hour )
+			return true
+		else if ( this.hour == time.hour ) {
+			if ( this.minute > time.minute )
+				return true;
+			else
+				return false;
+		} else
+			return false;
+	} else
+		return false;
+}
+
+OpeningHourTime.prototype.isBefore = function(time) {
+	if ( this.day < time.day )
+		return true;
+	else if ( this.day == time.day ) {
+		if ( this.hour < time.hour )
+			return true;
+		else if ( this.hour == time.hour ) {
+			if ( this.minute < time.minute )
+				return true;
+			else
+				return false;
+		} else
+			return false;
+	} else
+		return false;
+}
+
+/**
+ * OpeningHours
+ */
+function OpeningHours (ranges) {
+	this.ranges = ranges;
+}
+
+OpeningHours.prototype.isOpen = function (date) {
+	for(i=0;i<this.ranges.length;i++) {
+		if ( this.ranges[i].containsDate(date) )
+			return true;
+	}
+	return false;
+}
+
+exports.OpeningHourRange = OpeningHourRange;
+exports.OpeningHourTime = OpeningHourTime;
+exports.OpeningHours = OpeningHours;
---
+++ test/testOpeningHours.js
@@ -0,0 +1,221 @@
+/**
+ * testOpeningHours.js
+ *
+ * Node.js (mocha, expect.js) tests for OpeningHours module
+ * 
+ * Copyright: Tom Erik Støwer 2013
+ * E-mail: testower@gmail.com
+ *
+ */
+
+var OpeningHoursModule = require('../cloud/OpeningHours.js');
+var OpeningHourRange = OpeningHoursModule.OpeningHourRange;
+var OpeningHourTime = OpeningHoursModule.OpeningHourTime;
+var OpeningHours = OpeningHoursModule.OpeningHours;
+
+var expect = require('expect.js');
+
+describe('OpeningHours', function () {
+
+	describe('OpeningHourTime constructor', function () {
+		var mockAttributes = {
+			day: 0,
+			hour: 18,
+			minute: 30
+		};
+		var mockObject = new OpeningHourTime(mockAttributes);
+
+		it('should take day, hour and minute attributes in a json and set them properly', function (done) {
+			done();
+			expect(mockObject.day).to.equal(mockAttributes.day);
+			expect(mockObject.hour).to.equal(mockAttributes.hour);
+			expect(mockObject.minute).to.equal(mockAttributes.minute);
+		});
+	});
+
+	describe('OpeningHourRange', function () {
+		var startTime = new OpeningHourTime({
+			day: 0,
+			hour: 18,
+			minute: 30
+		});
+		var endTime = new OpeningHourTime({
+			day: 1,
+			hour: 7,
+			minute: 0
+		});
+		var mockObject = new OpeningHourRange({
+			start: startTime,
+			end: endTime
+		});
+
+		it('constructor should accept start and end time as OpeningHourTime objects in a json and set them properly', function (done) {
+			done();
+			expect(mockObject.start).to.equal(startTime);
+			expect(mockObject.end).to.equal(endTime);
+		});
+
+		describe('containsDate(date)', function () {
+
+			it('should return true if date is contained in range', function (done) {
+				done();
+				var date = new Date(2013,3,28,22,00);
+				expect(mockObject.containsDate(date)).to.be.ok();
+			});
+
+			it('should return false if date is not contained in range', function (done) {
+				done();
+				var date = new Date(2013,3,29,22,00);
+				expect(mockObject.containsDate(date)).not.to.be.ok();
+			});
+		});
+
+		describe('isAfter(time)', function () {
+			var timeToCheckAgainst = new OpeningHourTime({
+				day:0,
+				hour:18,
+				minute:30
+			});
+			var firstTimeToCheck = new OpeningHourTime({
+				day:0,
+				hour:18,
+				minute:31
+			});
+			var secondTimeToCheck = new OpeningHourTime({
+				day:0,
+				hour:19,
+				minute:30
+			});
+			var thirdTimeToCheck = new OpeningHourTime({
+				day:1,
+				hour:18,
+				minute:30
+			});
+
+			it('should return true if provided time is after objects time', function (done) {
+				done();
+				expect(firstTimeToCheck.isAfter(timeToCheckAgainst)).to.be.ok();
+				expect(secondTimeToCheck.isAfter(timeToCheckAgainst)).to.be.ok();
+				expect(thirdTimeToCheck.isAfter(timeToCheckAgainst)).to.be.ok();
+			});
+
+			var fourthTimeToCheck = new OpeningHourTime({
+				day:0,
+				hour:17,
+				minute:30
+			});
+			var fifthTimeToCheck = new OpeningHourTime({
+				day:0,
+				hour:18,
+				minute:29
+			});
+			var sixthTimeToCheck = new OpeningHourTime({
+				day:0,
+				hour:18,
+				minute:30
+			});
+
+			it('should return false if provided time is not after objects time', function (done) {
+				done();
+				expect(fourthTimeToCheck.isAfter(timeToCheckAgainst)).not.to.be.ok();
+				expect(fifthTimeToCheck.isAfter(timeToCheckAgainst)).not.to.be.ok();
+				expect(sixthTimeToCheck.isAfter(timeToCheckAgainst)).not.to.be.ok();
+			});
+		});
+
+		describe('isBefore(time)', function () {
+			var timeToCheckAgainst = new OpeningHourTime({
+				day:2,
+				hour:19,
+				minute:0
+			});
+			var firstTimeToCheck = new OpeningHourTime({
+				day:2,
+				hour:18,
+				minute:59
+			});
+			var secondTimeToCheck = new OpeningHourTime({
+				day:2,
+				hour:17,
+				minute:0
+			});
+			var thirdTimeToCheck = new OpeningHourTime({
+				day:1,
+				hour:17,
+				minute:0
+			});
+
+			it('should return true if provided time is before objects time', function (done) {
+				done();
+				expect(firstTimeToCheck.isBefore(timeToCheckAgainst)).to.be.ok();
+				expect(secondTimeToCheck.isBefore(timeToCheckAgainst)).to.be.ok();
+				expect(thirdTimeToCheck.isBefore(timeToCheckAgainst)).to.be.ok();
+			});
+
+			var fourthTimeToCheck = new OpeningHourTime({
+				day:2,
+				hour:19,
+				minute:0
+			});
+			var fifthTimeToCheck = new OpeningHourTime({
+				day:2,
+				hour:20,
+				minute:0
+			});
+
+			var sixthTimeToCheck = new OpeningHourTime({
+				day:3,
+				hour:19,
+				minute:0
+			});
+
+			it('should return false if provided time is not before objects time', function (done) {
+				done();
+				expect(fourthTimeToCheck.isBefore(timeToCheckAgainst)).not.to.be.ok();
+				expect(fifthTimeToCheck.isBefore(timeToCheckAgainst)).not.to.be.ok();
+				expect(sixthTimeToCheck.isBefore(timeToCheckAgainst)).not.to.be.ok();
+			});
+		});
+	});
+
+	describe('OpeningHours', function () {
+		var firstRange = new OpeningHourRange({
+			start: { day: 0, hour: 18, minute: 30 },
+			end: { day: 1, hour: 7, minute: 0 }
+		});
+		var secondRange = new OpeningHourRange({
+			start: { day: 1, hour: 18, minute: 30 },
+			end: { day: 2, hour: 7, minute: 0 }
+		});
+		var thirdRange = new OpeningHourRange({
+			start: { day: 2, hour: 18, minute: 30 },
+			end: { day: 3, hour: 7, minute: 0 }
+		});
+		var mockObject = new OpeningHours([firstRange, secondRange, thirdRange]);
+
+		it('constructor should take an array of ranges as its argument and set as its ranges attribute', function (done) {
+			done();
+			expect(mockObject.ranges).to.be.an(Array);
+			expect(mockObject.ranges[0]).to.be.equal(firstRange);
+		});
+
+		describe('isOpen', function () {
+
+			it('should return true if provided date is contained one of the ranges', function (done) {
+				done();
+				var date = new Date(2013,3,29,19,30);
+				expect(mockObject.isOpen(date)).to.be.ok();
+			});
+
+			it('should return false if provided date is not contained in one of the ranges', function (done) {
+				done();
+				var firstDate = new Date(2013,3,29,17,0);
+				var secondDate = new Date(2013,4,1,12,30);
+				var thirdDate = new Date(2013,3,26,22,0);
+				expect(mockObject.isOpen(firstDate)).not.to.be.ok();
+				expect(mockObject.isOpen(secondDate)).not.to.be.ok();
+				expect(mockObject.isOpen(thirdDate)).not.to.be.ok();
+			});
+		});
+	});
+});
